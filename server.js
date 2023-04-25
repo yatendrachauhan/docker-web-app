6c4625a398ad78efb815a0a30df7f4a95d25bea8
Yatendra Chauhan <yatendra.chauhan@nagarro.com>
2023-04-25 15:31:05 +0530

    Update page message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Demo app for testing docker and kubernates on GCP.');
 });
 
 app.listen(PORT, HOST, () => {
